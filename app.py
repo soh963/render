afbc188a3e2e14e3c0f212b488b470226d73a24f
Hossam Mohammed Al-Rifai <167508690+soh963@users.noreply.github.com>
2024-08-24 21:09:53 +0900

    render php 설정 오류 수정 다시 배포 app.py 테스트2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, Render with Docker and Flask! 안녕하세요"
+    return "Hello, Render with Docker and Flask! 안녕하세요 hi my name is kang"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
