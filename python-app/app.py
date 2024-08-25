edd5f1402be14a7a6d0734e4a21eb13ca40c66d3
Hossam Mohammed Al-Rifai <167508690+soh963@users.noreply.github.com>
2024-08-26 08:58:20 +0900

    php upload

--- python-app/app.py
+++ python-app/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 app = Flask(__name__)
 
@@ -6,4 +7,5 @@ def hello():
     return "Hello from Python Flask!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
