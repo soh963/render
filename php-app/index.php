c6b61fb1f916aeb79f0f724ec460b2752935f23d
Hossam Mohammed Al-Rifai <167508690+soh963@users.noreply.github.com>
2024-08-26 10:09:09 +0900

    php 업로드

--- php-app/index.php
+++ php-app/index.php
@@ -1,5 +1,4 @@
 <?php
-// 한글 인코딩 설정
 header('Content-Type: text/html; charset=UTF-8');
 mb_internal_encoding('UTF-8');
 mb_http_output('UTF-8');
@@ -7,6 +6,6 @@ mb_http_input('UTF-8');
 mb_language('uni');
 mb_regex_encoding('UTF-8');
 
-// 메인 콘텐츠
 echo "안녕하세요! PHP에서 보내는 한글 메시지입니다.";
+phpinfo();
 ?>
